60698ce6cfa822c844d27ef99da4c7e27288439c
Jaylon3 <114821808+Jaylon3@users.noreply.github.com>
2022-11-17 16:07:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
 // write your code here
+#include <iostream>
+
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int main()
+{
+	int N;
+	cout << "Enter random input:" << endl;
+	cin >> N;
+	
+	
+	for (int x = 1; x < N+1; x++) {
+		cout << "Output:" << rand() % 100 << endl;
+
+			
+	}
+
+
+	
+	
+
+	
+}
+
